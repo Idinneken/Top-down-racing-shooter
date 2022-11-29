b354a40bee904915a6e1d19bc519742568e88356
GEORGE NORMAN <2003314@uad.ac.uk>
2022-11-29 18:00:33 +0000

    Basic geo changes

--- Assets/Scripts/Objects/Game/Collectibles/AffectStatOnTrigger.cs
+++ Assets/Scripts/Objects/Game/Collectibles/AffectStatOnTrigger.cs
@@ -4,7 +4,7 @@ public class AffectStatOnTrigger : MonoBehaviour
 {            
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponent<InvokesTriggers>() != null && gameObject.GetComponent<Trigger>() != null && gameObject.GetComponent<Trigger>().triggerOnExit && other.gameObject.GetComponent<InvokesTriggers>().triggerTags.Contains(GetComponent<Trigger>().requiredInvokerTag))
+        if (other.gameObject.GetComponent<InvokesTriggers>() != null && gameObject.GetComponent<Trigger>() != null && gameObject.GetComponent<Trigger>().triggerOnEnter && other.gameObject.GetComponent<InvokesTriggers>().triggerTags.Contains(GetComponent<Trigger>().requiredInvokerTag))
         {
             Stats agentStats = other.gameObject.GetComponent<Stats>() ?? (Stats)other.gameObject.GetComponent<InvokesTriggers>().associatedComponent ?? null;
             AffectsStat affectsStat = GetComponent<AffectsStat>();
@@ -18,7 +18,7 @@ public class AffectStatOnTrigger : MonoBehaviour
 
     public void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.GetComponent<InvokesTriggers>() != null && gameObject.GetComponent<Trigger>() != null && gameObject.GetComponent<Trigger>().triggerOnEnter && other.gameObject.GetComponent<InvokesTriggers>().triggerTags.Contains(GetComponent<Trigger>().requiredInvokerTag))
+        if (other.gameObject.GetComponent<InvokesTriggers>() != null && gameObject.GetComponent<Trigger>() != null && gameObject.GetComponent<Trigger>().triggerOnExit && other.gameObject.GetComponent<InvokesTriggers>().triggerTags.Contains(GetComponent<Trigger>().requiredInvokerTag))
         {
             Stats agentStats = other.gameObject.GetComponent<Stats>() ?? (Stats)other.gameObject.GetComponent<InvokesTriggers>().associatedComponent ?? null;
             AffectsStat affectsStat = GetComponent<AffectsStat>();
