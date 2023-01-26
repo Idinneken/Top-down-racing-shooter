bf22441a2393d5ee8bd76e724a854265869e15f1
GEORGE NORMAN <2003314@uad.ac.uk>
2023-01-26 18:05:37 +0000

    Ready for iteration 2

--- Assets/Scripts/Objects/Game/Level.cs
+++ Assets/Scripts/Objects/Game/Level.cs
@@ -62,9 +62,6 @@ public class Level : MonoBehaviour
 
         winPanelObject.SetActive(true);
 
-        print(playerStats);
-        print(playerStats.rankScorePair);
-
         rankText.SetText(playerStats.rankScorePair.Key);
         scoreText.SetText(playerStats.stats["score"].value.ToString());
     }
